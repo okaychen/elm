8363b37bb90a776fb789e137b633ae9a1027b427
okaychen <1440281580@qq.com>
2017-11-23 21:48:38 +0800

    update app.vue

--- src/App.vue
+++ src/App.vue
@@ -6,40 +6,40 @@
 </template>
 
 <script>
-  import MHeader from '@/components/m-header/m-header'
-  import Tab from '@/components/tab/tab'
-  export default {
-    data () {
-      return {
-        msg: 'hello world'
-      }
-    },
-    components: {
-      MHeader,
-      Tab
-    }
-}
+import MHeader from "@/components/m-header/m-header";
+import Tab from "@/components/tab/tab";
+export default {
+  data() {
+    return {
+      msg: "hello world"
+    };
+  },
+  components: {
+    MHeader,
+    Tab
+  }
+};
 </script>
 
 <style>
-*{
+* {
   margin: 0;
   padding: 0;
 }
-body{
+body {
   background: #333;
 }
 
-li{
-  list-style:none;
+li {
+  list-style: none;
   float: left;
 }
-a{
+a {
   color: #ffe79e;
   text-decoration: none;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   padding: 5px 0;
