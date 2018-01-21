1799b67070131331ca4385acf9e31c0765dcdb0a
okaychen <1440281580@qq.com>
2018-01-21 20:50:44 +0800

    complete header vue

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -16,9 +16,20 @@
           <span class="icon" v-bind:class="classMap[seller.supports[0].type]"></span>
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
+        <div class="support-count" v-if="seller.supports">
+          <span class="count">{{seller.supports.length}}个</span>
+          <i class="icon-keyboard_arrow_right"></i>
+        </div>
       </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <!-- 公告 -->
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img v-bind:src="seller.avatar" alt="" width="100%" height="100%"/>
+    </div>
   </div>
 </template>
 
@@ -38,15 +49,17 @@ export default {
 <style lang="stylus" type="stylesheet/stylus">
 @import "../../common/stylus/mixin.styl";
   .header
+    position relative
     color #fff
-    background #000000
+    background rgba(7,17,27,0.5)
     .content-wrapper
+      position relative
       padding : 24px 12px 18px 24px
-      font-size 0 
+      font-size 0
       .avatar
         display inline-block
         vertical-align top
-        img 
+        img
           border-radius 2px
       .content
         display inline-block
@@ -93,4 +106,56 @@ export default {
           .text
             line-height 12px
             font-size 12px
+      .support-count
+        position absolute
+        right 12px
+        bottom 14px
+        padding 0 8px
+        height 24px
+        line-height 24px
+        border-radius 14px
+        background rgba(0,0,0,0.2)
+        text-align center
+        .count
+          vertical-align top
+          font-size 10px
+        .icon-keyboard_arrow_right
+          margin-left:2px
+          line-height 24px
+          font-size 10px
+
+    .bulletin-wrapper
+      position relative
+      height 28px
+      line-height 28px
+      padding 0 22px 0 12px
+      white-space nowrap
+      overflow hidden
+      text-overflow ellipsis
+      background rgba(7,17,27,0.1)
+      .bulletin-title
+        display inline-block
+        vertical-align top
+        margin-top 7px
+        width 22px
+        height 12px
+        bg-image('bulletin')
+        background-size 22px 12px
+        background-repeat no-repeat
+      .bulletin-text
+          margin 0 4px
+          font-size 10px
+      .icon-keyboard_arrow_right
+        position absolute
+        font-size 10px
+        right 12px
+        top 8px
+    .background
+      position absolute
+      top 0
+      left 0 
+      width 100%
+      height 100%
+      z-index -1
+      filter:blur(10px)
 </style>
