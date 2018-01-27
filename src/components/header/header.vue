69b05e1ca51af0256e09f2dd33ba6e66eb55caa9
okaychen <1440281580@qq.com>
2018-01-27 17:47:25 +0800

    update header‘s detail layout

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -36,7 +36,20 @@
       <div class="detail-wrapper clearfix">
         <div class="detail-main">
           <h1 class="name">{{seller.name}}</h1>
-          <star :size="48" :score="seller.score"></star>
+          <div class="star-wrapper">
+            <star :size="48" :score="seller.score"></star>
+          </div>
+          <div class="title">
+            <div class="line"></div>
+            <div class="text">优惠信息</div>
+            <div class="line"></div>
+          </div>
+          <ul v-if="seller.supports" class="supports">
+            <li class="support-item" v-for="(item,index) in seller.supports">
+              <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+              <span class="text">{{seller.supports[index].description}}</span>
+            </li>
+          </ul>
         </div>
       </div>
       <!-- stricky footer -->
@@ -201,10 +214,33 @@ export default {
       overflow auto
       background rgba(7,17,27,0.8)
       .detail-wrapper
+        width 100%
         min-height 100%
         .detail-main
           margin-top 64px
           padding-bottom 64px
+          .name
+            line-height 16px
+            text-align center
+            font-size 16px
+            font-weight 700
+          .star-wrapper
+            margin-top 18px
+            padding 2px 0px
+            text-align center
+          .title
+            display flex
+            width 80%
+            margin 28px auto 24px auto 
+            .line
+              flex 1
+              position relative
+              top -6px
+              border-bottom 1px solid rgba(255,255,255,0.2)
+            .text
+              padding 0 12px
+              font-weight 700
+              font-size 14px
       .detail-close
         position relative
         width 32px
