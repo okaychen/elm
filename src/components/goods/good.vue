6f111666b8a95c00f05b98973dd911b0641a501f
okaychen <1440281580@qq.com>
2018-01-28 18:32:27 +0800

    complete goods's layout

--- src/components/goods/good.vue
+++ src/components/goods/good.vue
@@ -9,7 +9,32 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper"></div>
+    <div class="foods-wrapper">
+      <ul>
+        <li v-for="item in goods" class="food-list">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" class="food-item border-1px">
+              <div class="icon">
+                <img width="57" heigh="57" :src="food.icon" />
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -87,4 +112,51 @@ export default {
           font-size 12px
     .foods-wrapper
       flex 1
+      .title
+        padding-left 14px
+        heigth 26px
+        line-height 26px
+        border-left 2px solid #d9dde1
+        font-size 12px
+        color rgb(147,153,159)
+        background #f3f5f7
+      .food-item
+        display flex
+        margin 18px
+        padding-bottom 18px
+        border-1px(rgba(7,17,27,0.1))
+        &:last-child
+          border-none()
+          margin-bottom 0
+        .icon
+          flex 0 0 57px
+          margin-right 10px
+        .content
+          flex 1
+          .name
+            margin 2px 0 8px 0px
+            height 14px
+            line-height 14px
+            font-size 14px
+            color rgb(7,17,27)
+          .desc, .extra
+            line-height 10px
+            font-size 10px
+            color rgb(147,153,159)
+          .desc
+            margin-bottom 8px
+          .extra
+            &.count
+              margin-right 12px
+          .price
+            font-weight 700
+            line-height 24px
+            .now
+              margin-right 8px
+              font-size 14px
+              color rgb(240,20,20)
+            .old
+              text-decoration line-through
+              font-size 10px
+              color rgb(147,153,159)
 </style>
