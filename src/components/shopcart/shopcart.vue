689c1396fd4e1c2fb41e33a5ffa683db683caf2b
okaychen <1440281580@qq.com>
2018-01-30 19:22:30 +0800

    complete the logic of shopcart

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -3,15 +3,16 @@
     <div class="content">
       <div class="content-left">
         <div class="logo-wrapper">
-          <div class="logo">
-            <span class="icon-shopping_cart"></span>
+          <div class="logo" :class="{'highlight':totalCount>0}">
+            <span class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></span>
           </div>
+          <div class="num" v-show="totalCount>0">{{totalCount}}</div>
         </div>
-        <div class="price">0元</div>
+        <div class="price" :class="{'highlight':totalPrice>0}">￥{{totalPrice}}元</div>
         <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
       </div>
       <div class="content-right">
-        <div class="pay">￥{{minPrice}}起送</div>
+        <div class="pay" :class="payClass">{{payDesc}}</div>
       </div>
     </div>
   </div>
@@ -20,6 +21,17 @@
 <script type="text/ecmascript-6">
   export default {
     props:{
+      selectFoods:{
+        type:Array,
+        default(){
+          return [
+           {
+              price:10,
+              count:2
+           }
+          ];
+        }
+      },
       deliveryPrice:{
         type:Number,
         default:0
@@ -27,6 +39,39 @@
       minPrice:{
         type:Number,
         default:0
+      }
+    },
+    computed:{
+      totalPrice() {
+        let total = 0;
+        this.selectFoods.forEach((food)=>{
+          total += food.price * food.count;
+        });
+        return total;
+      },
+      totalCount() {
+        let count = 0;
+        this.selectFoods.forEach((food)=>{
+            count += food.count;
+        });
+        return count;
+      },
+      payDesc() {
+        if(this.totalPrice === 0){
+          return `¥${this.minPrice}元起送`; 
+        }else if(this.totalPrice<this.minPrice){
+          let diff = this.minPrice - this.totalPrice;
+          return `还差¥${diff}元起送`
+        }else{
+          return '去结算';
+        }
+      },
+      payClass() {
+        if(this.totalPrice< this.minPrice){
+          return 'not-enough';
+        }else{
+          return 'enough';
+        }
       }
     }
   }
@@ -66,10 +111,28 @@
             border-radius 50%
             text-align center
             background #2b343c
+            &.highlight
+              background rgb(0,160,220)
             .icon-shopping_cart
               line-height 44px
               font-size 24px
               color #80858a
+              &.highlight
+                color #ffffff
+          .num
+            position absolute
+            top 0
+            right 0
+            width 24px
+            height 16px
+            line-height 16px
+            text-align center
+            border-radius 16px
+            font-size 9px
+            font-weight 700
+            color #ffffff
+            background rgb(240,20,20)
+            box-shadow 0px 4px 8px 0 rgba(0,0,0,0.4)
         .price
           display inline-block
           vertical-align top 
@@ -80,6 +143,8 @@
           border-right 1px solid rgba(255,255,255,0.1)
           font-size 16px
           font-weight 700
+          &.highlight
+            color #fff
         .desc
           display inline-block
           vertical-align top
@@ -96,4 +161,9 @@
           font-size 12px
           font-weight 700
           background #2b333b
+          &.not-enough
+            background #2b333b 
+          &.enough
+            background #00b43c
+            color #fff
 </style>
